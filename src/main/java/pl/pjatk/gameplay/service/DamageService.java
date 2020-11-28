c1831c4327fe914aefccb45ee1b1d1158d2ede0e
Marcin Żmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2020-11-28 19:01:25 +0100

    wyrownanie

--- src/main/java/pl/pjatk/gameplay/service/DamageService.java
+++ src/main/java/pl/pjatk/gameplay/service/DamageService.java
@@ -8,34 +8,34 @@ import pl.pjatk.gameplay.model.Player;
 public class DamageService {
 
 
-    public Player dealDamage(Player target,Player attacker ){
+    public Player dealDamage(Player target, Player attacker) {
         target.setHealth(target.getHealth() - attacker.getAttack());
         return target;
     }
 
-    public Player heal(Player player, int healValue){
-        if(player.getHealth() + healValue > 100){
+    public Player heal(Player player, int healValue) {
+        if (player.getHealth() + healValue > 100) {
             return player;
         }
         player.setHealth(player.getHealth() + healValue);
-        return  player;
+        return player;
     }
 
-    public Player debuff(Player player, int debufValue){
-        if(player.getAttack() - debufValue < 0){
+    public Player debuff(Player player, int debufValue) {
+        if (player.getAttack() - debufValue < 0) {
             return player;
         }
         player.setAttack(player.getAttack() - debufValue);
-        return  player;
+        return player;
     }
 
-    public Player buff(Player player, int buffValue){
+    public Player buff(Player player, int buffValue) {
         player.setAttack(player.getAttack() + buffValue);
-        return  player;
+        return player;
     }
 
-    public Player removeBuff(Player player, int buffValue){
+    public Player removeBuff(Player player, int buffValue) {
         player.setAttack(100);
-        return  player;
+        return player;
     }
 }
